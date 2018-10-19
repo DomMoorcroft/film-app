ac32fa021bee699751879dc56f0e673a3687420f
Dominic Moorcroft <moorcroftdominic@gmail.com>
2018-10-19 19:50:37 +0100

    Add main component and add it to app.js

---
+++ src/components/main/main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './main.css';
+
+class Main extends Component {
+    
+    render() {
+
+        return(
+
+            <div className="container">
+                <div className="row">
+                    <h1>Film App</h1>
+                </div>
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Main;
